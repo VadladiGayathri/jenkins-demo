12b74580d2875072239b78ed2b6585835b7e38a5
Gayathri <vadladigayathri@gmail.com>
2026-05-07 11:51:48 +0530

    First changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Jenkin new build changes");
+		System.out.println("Pavan is a waste fellow ");
 	}
 
 }
